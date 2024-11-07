da6ff9801f53c7488aa4c5e411ba7e95a1e09a00
MK <mike_live@mail.ru>
2024-11-07 19:48:23 +0300

    Version 0.0.2 was tested

--- src/regression_bias_corrector/_version.py
+++ src/regression_bias_corrector/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.2.dev0+ga164560.d20241107'
-__version_tuple__ = version_tuple = (0, 0, 2, 'dev0', 'ga164560.d20241107')
+__version__ = version = '0.0.3.dev3+gc788ee4'
+__version_tuple__ = version_tuple = (0, 0, 3, 'dev3', 'gc788ee4')
